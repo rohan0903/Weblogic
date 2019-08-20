da4bff553a69d37aaeffc1bf4b91db8091c2be3e
rohan0903 <50820181+rohan0903@users.noreply.github.com>
2019-08-20 19:04:28 +0530

    Add files via upload

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.controller.DemoController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Autowired
+	DemoController demoController;
+	
+	
+	@Test
+	public void contextLoads() throws UnsupportedOperationException{
+	}
+
+	
+	@Test
+	public void exampleTest()
+	{
+		String result=demoController.example();
+		
+		Assert.assertEquals(result, "HELLO-WORLD");
+		
+	}
+	@Test
+	public void exampleTest2()
+	{
+		String result=demoController.example();
+		
+		Assert.assertNotEquals(result, "HELLO");
+		
+	}
+	
+	
+}
